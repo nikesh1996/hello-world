3fa74b909f13938fb406eb57a08e7b99909413aa
nikesh1996 <nikeshchauhan1996@gmail.com>
2019-05-19 15:15:21 +0530

    Add files via upload

---
+++ mvc3/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Infosys.BioKartDAL;
+using Infosys.BioKartDAL.Models;
+using Infosys.BioKartMVC.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+
+
+
+namespace Infosys.BioKartMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSession();
+            services.AddAutoMapper(x => x.AddProfile(new BioKartMappers()));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<BioKartRepository>(new BioKartRepository(new BiokartContext(new DbContextOptions<BiokartContext>())));
+
+            services.AddDbContext<BiokartContext>
+              (options => options.UseSqlServer(Configuration.GetConnectionString("BioKartCon")));
+        }
+
+
+
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseSession(); 
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=home}/{action=index}/{id?}");
+            });
+        }
+    }
+}
